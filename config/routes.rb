ae2ec8f6e0d08bba8be305dd810f803ce78294fb
Anjana <anjana.lpn@gmail.com>
2019-10-18 14:36:06 +0100

    configure devise and add custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   resources :blogs do
     member do
       patch :toggle_status
     end
   end
+
   resources :portfolios
   get '/angular-items', to: 'portfolio#angular'
 
