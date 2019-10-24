5a2122bdbb9bda3ef60144add158e00f46c72e94
Anjana <anjana.lpn@gmail.com>
2019-10-24 13:52:58 +0100

    add session tracker for dynamic source links

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :portfolios
-  get '/angular-items', to: 'portfolio#angular'
+  resources :portfolios, except: [:show]
+  get '/portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get '/angular-items', to: 'portfolios#angular'
 
   get '/home',    to: 'pages#home'
   get '/about',   to: 'pages#about'
