b935fd3fffa392879e389f17ee00dcc6fe853bd9
Anjana <anjana.lpn@gmail.com>
2020-04-25 17:41:13 +0100

    build out initial form and partial call for comments along with controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+  end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
